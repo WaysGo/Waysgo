3d72c164854eed23157cab61eafdd611730229de
Maksim Milyutin <mmxckk@gmail.com>
2018-10-02 08:17:13 +0700

    Init

---
+++ js/fonts.js
@@ -0,0 +1,10 @@
+"use strict";
+
+WebFont.load({
+    google: {
+        families: [
+            'Nunito+Sans:300,800,800italic,300italic',
+            'Nunito+Sans:400,800,800italic,400italic',
+        ]
+    }
+});
